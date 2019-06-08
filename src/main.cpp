70c5ed597fdffd3a7cf368ebcd1d95e84bbc0365
Jefferson <jeovaneml@live.com>
2019-06-08 11:49:45 -0300

    Implementacao parcial do arquivo main

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,64 @@
 #include <iostream>
 #include <vector>
+#include <string.h>
+#include <stack>
+
 #include "vectorBuilder.hpp"
+#include "quickSort.hpp"
+#include "insertionSort.hpp"
+
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    vector<int>* vet;
+    string variation, vectorType;
+    int vectorSize, repetions = 20;
+    bool printUsedVector = false;
+
+    if(argc < 4 || argc > 5){
+        cerr << "Quantidade de parametros incorreta: 3 requeridos " << argc -1 << " fornecidos";
+        return 1;
+    }
+
+    variation = argv[1];
+    vectorType = argv[2];
+    vectorSize = stoi(argv[3]);
+
+    if(argc == 5)
+        if( strcmp(argv[4] , "-p") == 0)
+            printUsedVector = true;
+
 
     VectorBuilder constructor;
-    constructor.setVectorLength(500000);
-    constructor.setVectorOrder("OrdC");
-    vet = constructor.buildVector();
+    QuickSort sorter;
+    vector<int>* vet;
+    vector<int> usedVector;
+    stack<vector<int>> usedVectors;
 
-    for(int i = 0; i < vet->size(); i++)
-        cout << (*vet)[i] << endl;
+    constructor.setVectorLength(vectorSize);
+    constructor.setVectorOrder(vectorType);
+    sorter.setQuickSortType(variation);
 
-    cout << "Hello world!" << endl;
+    while(repetions > 0){
+        vet = constructor.buildVector();
+
+        if(printUsedVector)
+            usedVectors.push(*vet);
+
+        sorter.setVector(vet);
+        sorter.sort();
+
+        repetions--;
+    }
+
+    for(int i = 0; i < usedVectors.size(); i++){
+        usedVector = usedVectors.top();
+        usedVectors.pop();
+        for(int j = 0; j < usedVector.size(); j++){
+            cout << usedVector[j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
